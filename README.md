bdedecb119281c371f19a7b557b5e71891363cc4
eliseunogueira <eliseu.nogueira@outlook.com>
2022-06-24 10:12:44 -0300

    scratching the project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dio-desafio-js
+# Dio desafio javascript
 Desafio Javascript Contador e Todo-List
+
+# Contador (Como contar de 1 a 10 em chines)
+## *conter how to count from 1 to 10 in chinese*
---
+++ contador/assets/main.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  let number = 1;
+  document.getElementById("number").innerHTML = number;
+
+  let more = document
+    .getElementById("more")
+    .addEventListener("click", function () {});
+
+  let less = document
+    .getElementById("less")
+    .addEventListener("click", function () {});
+
+  let reset = document
+    .getElementById("reset")
+    .addEventListener("click", function () {});
+
+
+});
